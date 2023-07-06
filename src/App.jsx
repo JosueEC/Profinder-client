f62c39d288e2cd0ae90162597bd6156701e2cd70
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-07-06 10:43:07 -0600

    Se movio el ChakraProvider de App.jsx a main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import { ChakraProvider } from '@chakra-ui/react';
 import Navbar from "./components/navBar/Navbar.jsx";
 import HomePage from "../src/views/HomePage/HomePage"
 import { Routes, Route } from "react-router-dom";
@@ -8,13 +7,11 @@ import FromProvider from "./views/FromProvider/FromProvider";
 function App() {
   return (
     <div>
-      <ChakraProvider>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/login' element={<FromProvider />} />
-        </Routes>
-      </ChakraProvider>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<FromProvider />} />
+      </Routes>
     </div>
   )
 }
