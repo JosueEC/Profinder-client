547e9150a6ba3949145377455f417e8b8fb8fa23
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-07-06 15:45:52 -0600

    Definicion de la paleta de color y elementos para el componente UserLogin

--- src/views/UserLogin/UserLogin.jsx
+++ src/views/UserLogin/UserLogin.jsx
@@ -15,32 +15,47 @@ import {
   
   export default function SimpleCard() {
     return (
-      <Flex border='2px' borderColor='red' minH='100vh' align='center' justify='center' bg={useColorModeValue('gray.50', 'gray.800')}>
+      <Flex minH='100vh' align='center' justify='center' bg={useColorModeValue('gray.900', 'gray.800')}>
         <Stack spacing={8} mx='auto' maxW='lg' py={12} px={6}>
           <Stack align='center'>
-            <Heading fontSize='4xl'>Ingresa a tu cuenta</Heading>
-            <Text fontSize='lg' color='gray.600'>
-              para disfrutar de todos nuestros <Link color='blue.400'>servicios</Link>
+            <Heading fontSize='4xl' bgGradient='linear(to-l, teal.300, green.400)'bgClip='text'>
+              Hola de nuevo!
+            </Heading>
+            <Text fontSize='lg' color='gray.300'>
+              Ingresa para disfrutar de todos nuestros <Link color='teal.300'>servicios</Link>
             </Text>
           </Stack>
-          <Box rounded='lg' bg={useColorModeValue('white', 'gray.700')} boxShadow='lg' p={8}>
+          <Box rounded='lg' bg={useColorModeValue('blackAlpha.500', 'gray.700')} boxShadow='lg' p={8}>
             <Stack spacing={4}>
-              <FormControl id="email">
-                <FormLabel>Correo electronico</FormLabel>
-                <Input type="email" />
+              <FormControl id="email" color='gray.300'>
+                <FormLabel color='gray.300'>Correo electronico</FormLabel>
+                <Input type="email" focusBorderColor='teal.400' />
               </FormControl>
-              <FormControl id="password">
-                <FormLabel>Contraseña</FormLabel>
-                <Input type="password" />
+              <FormControl id="password" color='gray.300'>
+                <FormLabel color='gray.300'>Contraseña</FormLabel>
+                <Input type="password" focusBorderColor='teal.400'/>
               </FormControl>
               <Stack spacing={10}>
                 <Stack direction={{ base: 'column', sm: 'row' }} align='start' justify='space-between'>
-                  <Checkbox>Recordarme</Checkbox>
-                  <Link color='blue.400'>Olvidaste tu contraseña?</Link>
+                  <Checkbox color='gray.300'>Recordarme</Checkbox>
                 </Stack>
-                <Button bg='blue.400' color='white' _hover={{ bg: 'blue.500' }}>
-                  Ingresar
-                </Button>
+                <Stack spacing={5}>
+                  <Button bg='teal.50' color='black' _hover={{ bg: 'teal.100' }}>
+                    Google
+                  </Button>
+                  <Button bg='blue.600' color='white' _hover={{ bg: 'blue.700' }}>
+                    Facebook
+                  </Button>
+                  <Button bg='teal.400' color='white' _hover={{ bg: 'teal.500' }}>
+                    Ingresar
+                  </Button>
+                  <Text color='gray.300' letterSpacing='0.5px'>
+                    Aun no tienes una cuenta? Registrate gratis 
+                    <Link color='teal.300' ml='0.3rem'>
+                      aqui
+                    </Link>
+                  </Text>
+                </Stack>
               </Stack>
             </Stack>
           </Box>
