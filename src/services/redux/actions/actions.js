bb0618bed58a9af1cbe8291889db456ac83013bf
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-07-11 12:21:44 -0600

    Refacto de la funcion reducer, los filtros se unificaron en una sola action y case en el reducer

--- src/services/redux/actions/actions.js
+++ src/services/redux/actions/actions.js
@@ -2,12 +2,9 @@
 import { API, LOCAL } from '../../../utils/API/constants'
 import axios from 'axios'
 import {
-  FILTER_BY_CATEGORY,
   GET_ALL_SUPPLIERS,
   GET_CATEGORIES,
   SEARCH_PROFESSIONALS,
-  ORDER_BY_RATING,
-  FILTER_BY_GENRES,
   APPLY_FILTERS
 } from '../actionsTypes/actionsType'
 
@@ -44,20 +41,6 @@ const getAllCategories = () => {
   }
 }
 
-//! Filtrar por categoria
-const filterByCategory = (category) => {
-  return {
-    type: FILTER_BY_CATEGORY,
-    payload: category
-  }
-}
-
-const resetCategoryFilter = () => {
-  return {
-    type: 'RESET_CATEGORY_FILTER'
-  }
-}
-
 //! action para buscar por nombre de profesion
 export const searchProfessionals = (name) => {
   const URL = API.DOMAIN
@@ -73,22 +56,6 @@ export const searchProfessionals = (name) => {
         })
       })
       .catch((error) => console.error(error.message))
-  }
-}
-
-//!
-const orderByRating = (payload) => {
-  return {
-    type: ORDER_BY_RATING,
-    payload
-  }
-}
-
-//! Filter by Genres
-const filterByGenres = (payload) => {
-  return {
-    type: FILTER_BY_GENRES,
-    payload
   }
 }
 
@@ -128,10 +95,6 @@ const postProveedor = (info) => {
 export {
   getAllSuppliers,
   getAllCategories,
-  filterByCategory,
-  resetCategoryFilter,
-  orderByRating,
-  filterByGenres,
   postProveedor,
   applyFilters
 }
--- src/services/redux/reducer/reducer.js
+++ src/services/redux/reducer/reducer.js
@@ -1,10 +1,7 @@
 import {
   GET_ALL_SUPPLIERS,
   GET_CATEGORIES,
-  FILTER_BY_CATEGORY,
   SEARCH_PROFESSIONALS,
-  FILTER_BY_GENRES,
-  ORDER_BY_RATING,
   APPLY_FILTERS
 } from '../actionsTypes/actionsType'
 import { filterSuppliers } from '../filters/reduxFilters'
@@ -41,49 +38,11 @@ const reducer = (state = initialState, action) => {
         ...state,
         filters: { ...state.filters, [action.payload.filter]: action.payload.value }
       }
-    case FILTER_BY_CATEGORY: {
-      const filteredCategories = state.categories.filter(
-        (category) => category.nombre === action.payload
-      )
-      return {
-        ...state,
-        filteredCategories
-      }
-    }
     case SEARCH_PROFESSIONALS:
       return {
         ...state,
         professionals: action.payload
       }
-      //! filtro por rating
-    case ORDER_BY_RATING: {
-      const sortedProfessionals = [...state.backup].sort((a, b) => {
-        if (action.payload === 'lower') {
-          return a.rating - b.rating
-        } else if (action.payload === 'higher') {
-          return b.rating - a.rating
-        }
-        return 0
-      })
-
-      return {
-        ...state,
-        suppliers: [...sortedProfessionals]
-      }
-    }
-    //! filtro de busqueda por generos
-    case FILTER_BY_GENRES: {
-      const backup = state.backup
-      const filteredSuppliers =
-        action.payload === 'All'
-          ? backup
-          : backup.filter((supplier) => supplier.genre === action.payload)
-
-      return {
-        ...state,
-        suppliers: filteredSuppliers
-      }
-    }
     //! caso por default
     default:
       return { ...state }
