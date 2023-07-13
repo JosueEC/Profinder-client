8841fa45b26f0255b01c8828bc02d413f9c74ed4
Agustin Boasso <boassoagustin@gmail.com>
2023-07-13 12:50:25 -0300

    edit client

--- src/views/DashboardClient/DashboardClient.jsx
+++ src/views/DashboardClient/DashboardClient.jsx
@@ -1,17 +1,20 @@
-import EditClient from "../../components/DashboardClient/EditClient";
-import NavbarDashboardClient from "../../components/DashboardClient/NavbarDashboardClient";
-import SidebarClient from "../../components/DashboardClient/SidebarClient";
-
+import { Flex, Spacer } from '@chakra-ui/react';
+import EditClient from '../../components/DashboardClient/EditClient';
+import NavbarDashboardClient from '../../components/DashboardClient/NavbarDashboardClient';
+import SidebarClient from '../../components/DashboardClient/SidebarClient';
 
 const DashboardClient = () => {
   return (
     <div>
-      <NavbarDashboardClient/>
-      <br/>
-      <SidebarClient/>
-      <EditClient/>
+      <NavbarDashboardClient />
+      <br />
+      <Flex>
+        <SidebarClient />
+        <Spacer  />
+        <EditClient />
+      </Flex>
     </div>
-  )
-}
+  );
+};
 
-export default DashboardClient
+export default DashboardClient;
