d79b42e43070155c62af42345f26eb8cd1770c08
Agustin Boasso <boassoagustin@gmail.com>
2023-07-14 12:40:04 -0300

    add

--- src/views/DashboardClient/DashboardClient.jsx
+++ src/views/DashboardClient/DashboardClient.jsx
@@ -1,13 +1,13 @@
 import { Flex, Spacer } from '@chakra-ui/react';
 import EditClient from '../../components/DashboardClient/EditClient';
-import NavbarDashboardClient from '../../components/DashboardClient/NavbarDashboardClient';
+
 import SidebarClient from '../../components/DashboardClient/SidebarClient';
 
 const DashboardClient = () => {
   return (
     <div>
-      <NavbarDashboardClient />
-      <br />
+      
+      
       <Flex>
         <SidebarClient />
         <Spacer  />
