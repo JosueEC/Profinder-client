c38a289bb838f3f263d3c305c7fe72e858796a31
Agustin Boasso <boassoagustin@gmail.com>
2023-07-17 13:35:02 -0300

    commit

--- src/components/DashboardClient/HelpClient/HelpClient.jsx
+++ src/components/DashboardClient/HelpClient/HelpClient.jsx
@@ -3,12 +3,12 @@ import { Box, Text } from '@chakra-ui/react';
 
 const HelpClient = () => {
   return (
-    <Box width="100%" border="1px solid black" bg="gray.100">
+    <Box width="100%" bg="gray.100" border="1px solid black" borderRadius="md" p={4}>
       <Text fontSize="xl" fontWeight="bold" textAlign="center" color="teal.500">
-        Sección de Ayuda y Preguntas y Respuestas
+        Ayuda y Q & A
       </Text>
     </Box>
   );
-};git 
+};
 
 export default HelpClient;
