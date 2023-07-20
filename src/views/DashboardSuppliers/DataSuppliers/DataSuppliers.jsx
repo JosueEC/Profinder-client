3fa93f46f87916ec7f0e4ca950f99ac6334325c2
Maria Jose Olaechea <olaecheamariajose@gmail.com>
2023-07-20 15:55:53 -0300

    cambios

--- src/views/DashboardSuppliers/DataSuppliers/DataSuppliers.jsx
+++ src/views/DashboardSuppliers/DataSuppliers/DataSuppliers.jsx
@@ -2,21 +2,57 @@ import { Doughnut } from "react-chartjs-2";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getProfesionals } from "../../../services/redux/actions/actions";
+import axios from "axios";
 
 const DataSuppliers = () => {
   const dataSuppliers = useSelector((state) => state.profesionales);
   const userSession = JSON.parse(localStorage.getItem("userSession"));
   const profile = dataSuppliers.find((user) => user.id === userSession.id);
-  console.log(profile);
-
+  
   const dispatch = useDispatch();
+
 
   useEffect(() => {
     dispatch(getProfesionals());
   }, []);
 
+  useEffect(() => {
+    // Obtén la URL actual
+    dispatch(getProfesionals());
+    const currentUrl = window.location.href;
+
+    // Extrae los parámetros de la URL
+    const urlParams = new URLSearchParams(currentUrl);
+
+    // Obtén los datos que necesitas
+    const collectionStatus = urlParams.get('collection_status');
+    const preferenceId = urlParams.get('preference_id');
+
+    // Aquí puedes utilizar la información como desees
+    console.log('collectionStatus:', collectionStatus);
+    console.log('preferenceId:', preferenceId);
+    
+    // Verifica si collectionStatus es "approved"
+    if (collectionStatus === 'approved') {
+      // Enviar los datos al backend en un JSON mediante una solicitud POST
+      axios.post('https://backprofinder-production.up.railway.app/premium', {
+        collectionStatus: collectionStatus,
+        preferenceId: preferenceId,
+      })
+      .then((response) => {
+        console.log('Respuesta del backend:', response.data);
+        // Aquí puedes manejar la respuesta del backend, si es necesario
+      })
+      .catch((error) => {
+        console.error('Error al enviar datos al backend:', error);
+        // Aquí puedes manejar errores en caso de que ocurran
+      });
+    }
+  }, []);
+
   // hay que validar que exista la propiedad porque si no sale undefined, validar con todos los campos
   const numPosts = profile && profile.posts ? profile.posts.length : 0;
+  
 
   // const serviciosActivos = profile && profile.servicios_activos ? profile.servicios_activos : 0;
   const serviciosActivos = 20;
