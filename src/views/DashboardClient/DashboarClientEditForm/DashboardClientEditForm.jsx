b4902dcab7cbdea2d710f3cce274982bfa02f747
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-07-23 19:05:11 -0300

    dashboard

--- src/views/DashboardClient/DashboarClientEditForm/DashboardClientEditForm.jsx
+++ src/views/DashboardClient/DashboarClientEditForm/DashboardClientEditForm.jsx
@@ -1,4 +1,4 @@
-import { Flex, Spacer } from '@chakra-ui/react';
+import { Flex,Box,Grid, Spacer } from '@chakra-ui/react';
 import EditClient from '../../../components/DashboardClient/EditClient/EditClient';
 import SidebarClient from '../../../components/DashboardClient/SidebarClient/SidebarClient';
 
@@ -12,6 +12,12 @@ const DashboardClientEditForm = () => {
 
       <EditClient/>
     </Flex>
+    // <Box display={{ base: 'grid', lg: 'flex' }} height="100vh">
+    //   <Grid templateRows="5% 95%" display={{ base: 'grid', sm: 'grid',md: 'flex', lg: 'flex'}} width='100%'>
+    //     <SidebarClient />
+    //     <EditClient/>
+    //   </Grid>
+    // </Box>
   );
 };
 
