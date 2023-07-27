e23cba5615feb976ae6ffc5554eaf16617d00997
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-07-26 21:23:03 -0300

    detail profesional format - reseñas row pending md - trabajos center lg pending

--- src/views/DetailsSupplier/DetailsSupplier.jsx
+++ src/views/DetailsSupplier/DetailsSupplier.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link, useParams } from "react-router-dom";
+import { ArrowBackIcon } from '@chakra-ui/icons'
 import {
   Box,
   Heading,
@@ -61,47 +62,42 @@ const ArticleList = () => {
     transform: "scale(1.01)",
     cursor: "pointer",
   };
+  
+  // const bgColor = useColorModeValue("gray.800", "gray.500")
+  // const bgColorTwo = useColorModeValue("blackAlpha.800", "gray.800")
+  // const bgColorThree = useColorModeValue("teal.500", "teal.400")
+
+  const bgColor = useColorModeValue("gray.500","gray.800")
+  const bgColorTwo = useColorModeValue("gray.800","blackAlpha.800")
+  const bgColorThree = useColorModeValue("teal.400","teal.500")
+  const textColor = useColorModeValue('gray.100', 'white');
+  const textColorSection = useColorModeValue('gray.900', 'gray.100');
 
   if (!profesionalId) {
     return <div>Loading...</div>;
   }
+  // console.log(profesionalId[0])
 
   return (
     <>
-      <Spacer h="100px" bg={useColorModeValue("gray.800", "gray.500")} />
+      <Spacer h="100px"  bg={bgColor}/>
 
       <Container
         key={profesionalId.id}
         // color="gray.300"
-        bg={useColorModeValue("gray.800", "gray.500")}
+          bg={bgColor}
         maxW="100%"
         // py="5"
-
         px={{ base: "2", md: "8", lg: "10rem" }}
         align={"center"}
         justify={"center"}
       >
         {profesionalId.length === 0 ? (
-          <Spinner
-            thickness="4px"
-            speed="0.65s"
-            emptyColor="gray.200"
-            color="blue.500"
-            size="lg"
-          />
-        ) : (
-          <ScaleFade initialScale={0.9} in>
-            <Box>
-              <Flex
-                direction={{ base: "column", md: "row" }}
-                justify="center"
-                align="center"
-                // mt={8}
-                gap={{ base: "1rem", md: "3rem", lg: "3rem" }}
-                maxW={{ base: "full", md: "900px" }}
-                w={{ base: "full", md: "900px" }}
-              >
-                <Box flex={{ base: "1", md: "2" }}>
+          <Spinner thickness="4px" speed="0.65s" emptyColor="gray.200" color="blue.500" size="lg" />) : 
+        (<ScaleFade initialScale={0.9} in>
+          <Box>
+            <Flex direction={{ base: "column"}}  justify="center" align="center" gap={{ base: "1rem", md: "1rem", lg: "1rem" }} maxW={{ base: "full", md: "900px" }} width="100%" border='2px solid red' >
+                <Box flex={{ base: "1", md: "2" }} alignSelf='flex-start'>
                   <Link to="/categories">
                     <Button
                       bg="teal.400"
@@ -112,59 +108,18 @@ const ArticleList = () => {
                       order={{ base: 2, md: 1 }}
                       alignSelf={{ base: "center", md: "auto" }}
                     >
-                      Volver
+                      {/* Volver */}
+                      <ArrowBackIcon/>
                     </Button>
                   </Link>
                 </Box>
                 {profesionalId.map(
-                  ({
-                    id,
-                    name,
-                    email,
-                    image,
-                    ubication,
-                    description,
-                    professions,
-                    years_exp,
-                    genre,
-                    phone,
-                    posts,
-                  }) => (
-                    <Box
-                      key={id}
-                      rounded={{ base: "none", md: "md" }}
-                      boxShadow={"2xl"}
-                      align={"center"}
-                      _hover={hoverStyles}
-                      maxW={{ base: "100%", md: "500px" }}
-                      mx={{ base: "auto", md: "0" }}
-                      mb={{ base: "3rem", md: "0" }}
-                      flex={{ base: "1", md: "2" }}
-                      bg={useColorModeValue("blackAlpha.800", "gray.800")}
-                    >
-                      <Image
-                        src={image || NoAvatar}
-                        loading="lazy"
-                        alt="Image"
-                        boxSize={{ base: "300px", md: "auto" }}
-                        maxW={{ base: "300px", md: "100%" }}
-                        maxH="300px"
-                        objectFit="contain"
-                        marginTop="5"
-                        borderRadius="10px"
-                      />
-                      <Stack
-                        direction="column"
-                        spacing={2}
-                        p={8}
-                        align="center"
-                        textTransform={"uppercase"}
-                        fontWeight={700}
-                        fontSize={{ base: "lg", md: "2xl" }}
-                        letterSpacing={1.1}
-                        textAlign="center"
-                      >
-                        <Heading as="h1" textTransform="uppercase">
+                  ({ id, name, email, image, ubication, description, professions, years_exp, genre, phone, posts, }) => 
+                  (
+                    <Box key={id} rounded={{ base: "none", md: "md" }} boxShadow={"2xl"} align={"center"} _hover={hoverStyles} maxW={{ base: "100%", md: "500px" }} mx={{ base: "auto", md: "0" }} mb={{ base: "3rem", md: "0" }} flex={{ base: "1", md: "2" }} bg={bgColorTwo} >
+                      <Image src={image || NoAvatar} loading="lazy" alt="Image" boxSize={{ base: "300px", md: "auto" }} maxW={{ base: "300px", md: "100%" }} maxH="300px" objectFit="contain" marginTop="5" borderRadius="10px" />
+                      <Stack direction="column" spacing={2} p={8} align="center" textTransform={"uppercase"} fontWeight={700} fontSize={{ base: "lg", md: "2xl" }} letterSpacing={1.1} textAlign="center" >
+                        <Heading as="h1" textTransform="uppercase" color={textColor}>
                           {name || "Sin nombre"}
                         </Heading>
                         <InfoLabel textLabel={genre} iconLabel={FaUserAlt} />
@@ -172,16 +127,10 @@ const ArticleList = () => {
                         <InfoLabel textLabel={email} iconLabel={FaMailBulk} />
                         <InfoLabel textLabel={phone} iconLabel={FaPhone} />
                         <Box>
-                          <Text fontSize="16px">Años de experiencia:</Text>
+                          <Text fontSize="16px" color={textColor}>Años de experiencia:</Text>
                           <InfoLabel fontSize="20px" textLabel={years_exp} />
                         </Box>
-                        <Button
-                          onClick={handleChatToggle}
-                          bg={useColorModeValue("teal.500", "teal.400")}
-                          color="white"
-                          _hover={{ bg: "teal.600" }}
-                          leftIcon={<Icon as={FaRegPaperPlane} />}
-                        >
+                        <Button onClick={handleChatToggle} bg={bgColorThree} color="white" _hover={{ bg: "teal.600" }} leftIcon={<Icon as={FaRegPaperPlane} />}>
                           Contactar
                         </Button>
                       </Stack>
@@ -194,30 +143,26 @@ const ArticleList = () => {
                   )}
                 </Box>
               </Flex>
-              <Wrap spacing="50px" justify="center" align="center">
-                <SupplierReview profesionalId={id} key={profesionalId.id} />
-              </Wrap>
-              <Divider my={{ base: 8, md: 16 }} />
             </Box>
 
             <Box>
-              <Flex
-                direction="column"
-                align="center"
-                justify="center"
-                gap={{ base: "1rem", md: "3rem", lg: "3rem" }}
-              >
-                <Heading
-                  as="h2"
-                  textTransform="uppercase"
-                  color="blackAlpha.800"
-                >
+              {/* Profesional posteos */}
+              <Flex direction="column" align="center" justify="center" gap={{ base: "1rem"}} my={{ base: 8, md: 16 }}>
+                <Heading as="h2" textTransform="uppercase" color={textColorSection}>
                   Trabajos Recientes
                 </Heading>
                 <Divider my={2} />
-                <Wrap spacing="50px" justify="center">
-                  <SupplierPost profesionalId={id} key={profesionalId.id} />
-                </Wrap>
+                  <Wrap spacing="50px" justify="center">
+                    <SupplierPost profesionalId={id} key={profesionalId.id} />
+                  </Wrap>
+              </Flex>
+            </Box>
+            <Box>
+              <Heading as="h2" textTransform="uppercase" color={textColorSection}> Reseñas del profesional</Heading>
+              <Divider my={2} />
+              
+              <Flex justifyContent='center' alignItems='center' >
+                <SupplierReview profesionalId={id} key={profesionalId.id} />
               </Flex>
             </Box>
           </ScaleFade>
